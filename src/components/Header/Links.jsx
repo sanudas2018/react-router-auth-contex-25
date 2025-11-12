f7a9844577e1281ef9211ab8abe5475e0df42ccb
sanudas2018 <sanu.das08103@gmail.com>
2025-11-12 21:36:42 +0600

    Route এ link গুলি active এবং Links গুলি আলাদা করা হয়েছে।

---
+++ src/components/Header/Links.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router";
+import "./Links.css";
+// Step-4 : এই ফিইল এর কাজ টি এভাবেই করতে হবে।
+// Name Link Setup:
+
+export const links = (
+  <>
+    <li>
+      <NavLink className="mr-5 p-3 rounded-2xl" to="/">
+        {" "}
+        Home{" "}
+      </NavLink>
+    </li>
+    <li>
+      <NavLink className="mr-5 p-3 rounded-2xl" to="/about">
+        {" "}
+        About{" "}
+      </NavLink>
+    </li>
+    <li>
+      <NavLink className="mr-5 p-3 rounded-2xl" to="/post">
+        {" "}
+        Post{" "}
+      </NavLink>
+    </li>
+  </>
+);
